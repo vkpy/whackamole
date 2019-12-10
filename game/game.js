01d77c7b80f9f24cab30ec9a4850010d7cfce692
Vijay Krishna <vkpy24@gmail.com>
2019-12-10 20:57:43 +0700

    timer update

--- game/game.js
+++ game/game.js
@@ -2,6 +2,8 @@
 const holes = document.querySelectorAll('.hole');
 const scoreBoard = document.querySelector('.score');
 const moles = document.querySelectorAll('.mole');
+const time = document.querySelector('.displayTimeLeft');
+const endTime = document.querySelector('.displayEndTime')
 let lastHole;
 let timeUp = false;
 let score = 0;
@@ -22,7 +24,7 @@ function randomHole(holes) {
 }
 
 function peep() {
-  const time = randomTime(200, 1000);
+  const time = randomTime(500, 1200);
   const hole = randomHole(holes);
   hole.classList.add('up');
   setTimeout(() => {
@@ -38,7 +40,8 @@ function startGame() {
   peep();
   setTimeout(() => {
     timeUp = true;
-  }, 20000);
+  }, 60000);
+  timer(60);
 }
 
 function bonk(e) {
@@ -49,3 +52,40 @@ function bonk(e) {
 }
 
 moles.forEach(mole => mole.addEventListener('click', bonk));
+
+
+let countdown;
+function timer(seconds) {
+  const now = Date.now();
+  const then = now + seconds * 1000;
+  displayTimeLeft(seconds);
+
+  countdown = setInterval(() => {
+    // const adjustedTime = { 00: 00 };
+    const secondsLeft = Math.round((then - Date.now()) / 1000);
+    // stop the timer if it is less than 0
+    if (secondsLeft < 0) {
+      clearInterval(countdown);
+      return;
+    }
+    // display the time
+    displayTimeLeft(secondsLeft);
+  }, 1000);
+}
+
+function displayTimeLeft(seconds) {
+  const minutes = Math.floor(seconds / 60);
+  const remainderSeconds = seconds % 60;
+  const display = `${minutes}:${remainderSeconds < 10 ? '0' : ''}${remainderSeconds}`;
+  time.style = `"height: 11rem;
+                width: 20rem;
+                border: 4px solid black;
+                margin: 0.4em 6em;
+                font-size: 6em"`
+
+  if (remainderSeconds === 0) {
+    time.textContent = "Game Over!!"
+  } else {
+    time.textContent = display;
+  }
+}
